e5ce130c8d086503a308e70e50825a91ca3e6f8f
pschow <paul.schow@gmail.com>
2014-07-10 11:51:44 -0600

    adding db and sound into walkin.py

--- sounddbtest1.py
+++ sounddbtest1.py
@@ -1,21 +1,19 @@
 import sqlite3
 import pygame  # for audio
 import time
-#import wave
-
-column_name = 'Last'
 
 # connect to the database
 conn = sqlite3.connect('gone.db')
 # the cursor is c
 c = conn.cursor()
 
+# initalize pygame mixer for audio
 pygame.mixer.init()
 
 #print 'Searching in', column_name
 #search = raw_input("Enter Search Term: ")
-search = 0  # 1 is the last marker
-query = "SELECT * FROM gone WHERE last=? ORDER BY {0}".format(column_name)
+search = 1  # 1 is the last marker
+query = "SELECT * FROM gone WHERE last=? ORDER BY {0}".format('Last')
 c.execute(query, (search,))
 for row in c:
     print row
