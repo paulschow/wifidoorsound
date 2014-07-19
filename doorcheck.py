8a010fbd96504113efa3ce53ea5de7401eb93425
pschow <paulschow@gmail.com>
2014-07-19 15:18:06 -0600

    Rewrote doorcheck.py, now it works

--- doorcheck.py
+++ doorcheck.py
@@ -1,8 +1,14 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 import sqlite3
 import pygame
 import time
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Set pin 14 as GPIO input with a pull up resistor
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # connect to the database
 conn = sqlite3.connect('gone.db')
@@ -12,38 +18,26 @@ c = conn.cursor()
 # initalize pygame mixer for audio
 pygame.mixer.init()
 
+#counter = 0
+while True:
+    # Wait for the rising edge
+    # AKA detect when something gets too close
+    print "Waiting for sensor event"
+    GPIO.wait_for_edge(14, GPIO.RISING)
+    print ' Object Detected '
+    search = 1  # 1 is the last marker
+    query = "SELECT * FROM gone WHERE last=? ORDER BY {0}".format('Last')
+    c.execute(query, (search,))
+    for row in c:
+        #print row
+        print 'Last person was:', row[4]
+        print 'MP3 file is:', row[3]
+        pygame.mixer.music.load(row[3])  # load the file for the person
+        pygame.mixer.music.play()  # play the loaded file
+        #time.sleep(10)
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(0)
+        print "Sound played! \n"
+        #print counter
 
-def play_song(song):
-    # Let's play their song
-    print 'MP3 file is:', song
-    pygame.mixer.music.load(song)  # load the file for the person
-    pygame.mixer.music.play()  # play the loaded file
-    time.sleep(30)
-    #pygame.mixer.music.stop()
-    #while pygame.mixer.music.get_busy():
-    #    pygame.time.Clock().tick(0)
-
-#Main loop
-if __name__ == '__main__':
-    c.execute("SELECT * FROM gone")
-    rows = c.fetchall()
-    countrow = len(rows)  # Counts the number of rows
-    print "Number of Rows:", countrow
-    for row in rows:
-        print row[1]
-        #status = verbose_ping(row[5])  # ping the IP, get status
-        # 1 is here, 0 is gone or error
-        last = row[1]
-        print "Last is %d" % last
-        if last == 1:
-            #print "They're here!"
-            print "The Last Person Was %s" % row[4]
-            play_song(row[3])
-            # Send the row to db_here
-            #db_here(row[0], row[2], row[3])
-            #print "Total number of rows updated :", conn.total_changes
-        else:
-            #print "Not Here"
-            # Send the row to db_gone
-            print "Not Here"
-            #db_gone(row[0])
+GPIO.cleanup()
