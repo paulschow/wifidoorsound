7d72d0dbfed8935f751897fdf5264d62c92e9040
Paul Schow <paulschow@gmail.com>
2014-07-23 23:09:36 -0600

    Trying to improve speed

--- bluepinger.py
+++ bluepinger.py
@@ -3,23 +3,23 @@
 # Modified from doorpinger.py
 # This one uses bluetooth instead of wifi
 
+# You have to pair the devices and add them to macs.db
+
 # Note: Requires sqlite3 I think
 
-import os
-import socket
-import struct
-import select
-import time
+#import os
+#import socket
+#import struct
+#import select
+#import time
 import sqlite3
 import subprocess
 import re
-
 
 # connect to the database
 conn = sqlite3.connect('macs.db')
 # the cursor is c
 c = conn.cursor()
-
 
 #log = open('track1.txt', 'w')  # open a text file for logging
 #print log  # print what the log file is
@@ -30,6 +30,8 @@ def l2ping(phonemac):
 # Uses l2ping linux command
 # Returns a 0 if mac is gone
 # Returns a 1 if mac is here
+# This takes ~5 seconds per device, there's probably a better way
+# Probably takes even longer on rpi
     #p = os.popen("sudo l2ping -t 1 -c 1 E8:99:C4:D8:C9:91")
     #subprocess.call("ls")
     #ping = p.read()
@@ -77,15 +79,22 @@ def regping(ping):
 
 
 # function for marking someone as gone in the db
-def db_gone(keyid):
-    #print "key = %d" % keyid
-    #c.execute("SELECT * FROM gone")
-    c.execute("UPDATE gone SET Status = 0 WHERE key = %d" % keyid)
-    conn.commit()  # commit changes to the db
-    #print "Total number of rows updated :", conn.total_changes
+def db_gone(keyid, prestatus):
+    if prestatus == 0:
+        # Already marked as gone
+        # Do nothing
+        return
+    else:
+        # They where here before
+        # Mark them as gone
+        #print "key = %d" % keyid
+        #c.execute("SELECT * FROM gone")
+        c.execute("UPDATE gone SET Status = 0 WHERE key = %d" % keyid)
+        conn.commit()  # commit changes to the db
+        #print "Total number of rows updated :", conn.total_changes
 
 
-def db_here(keyid, prestatus, song):
+def db_here(keyid, prestatus):
     #print "key = %d" % keyid
     #print "Previous Status is  = %d" % prestatus
     if prestatus == 0:
@@ -115,16 +124,16 @@ if __name__ == '__main__':
         print "Number of Rows:", countrow
         x = countrow
         for row in rows:
-            #print "IP = %s" % row[5]
+            #print "MAC = %s" % row[5]
             #print "Name = %s" % row[4]
-            status = l2ping(row[5])  # ping the IP, get status
+            status = l2ping(row[5])  # ping the MAC, get status
             # 1 is here, 0 is gone or error
             #print "status is %s" % status
             if status == 1:
                 #print "They're here!"
                 print "\033[94m %s is Here \033[00m" % row[4]
                 # Send the row to db_here
-                db_here(row[0], row[2], row[3])
+                db_here(row[0], row[2])
                 print " "
                 #print "Total number of rows updated :", conn.total_changes
             else:
@@ -132,6 +141,6 @@ if __name__ == '__main__':
                 # Send the row to db_gone
                 print "\033[91m %s is Not Here \033[00m" % row[4]
                 print " "
-                db_gone(row[0])
+                db_gone(row[0], row[2])
         counter = counter + 1
         print "\033[33m Done \033[00m \n"
